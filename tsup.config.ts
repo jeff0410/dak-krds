68f0d671d1abeeb4dfdd23ce3af9759f4d1c89f0
jeff0410 <pazwsx@naver.com>
2026-01-27 10:23:40 +0900

    Feat:빌드 수정

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,20 +3,25 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
-  dts: true,
+  dts: false,
   splitting: false,
   sourcemap: true,
   clean: true,
-  external: ['react', 'react-dom'],
+  external: ['react', 'react-dom', '@mui/material', '@fullcalendar/daygrid', '@fullcalendar/interaction', '@fullcalendar/react', 'material-react-table'],
   treeshake: true,
-  minify: true,
+  minify: false,
   esbuildOptions(options) {
     options.banner = {
       js: '"use client";',
     };
+    options.loader = {
+      '.css': 'empty',
+      '.svg': 'empty',
+      '.png': 'empty',
+      '.jpg': 'empty',
+      '.jpeg': 'empty',
+      '.gif': 'empty',
+      '.webp': 'empty',
+    };
   },
-  loader: {
-    '.css': 'css',
-  },
-  injectStyle: true,
 });
